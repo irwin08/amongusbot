06ae2ffa46227370343254dc361807b5c62bd97a
Irwin08 <jesse.irwin11@gmail.com>
2021-06-18 17:47:28 -0700

    initial commit

---
+++ scheduler.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+class Scheduler:
+    # has_many Schedulee
+    pass
+
+class Schedulee:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
